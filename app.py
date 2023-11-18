e3b7b4f19b373697c14d6453cf805b00c321fa63
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2023-11-19 02:17:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, app, jsonify, url_for, render_template
+import pickle
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+import nltk
+
+app = Flask(__name__)
+
+ps= PorterStemmer()
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+def transform_text(text):
+  text= text.lower()
+  text = nltk.word_tokenize(text)
+  y = []
+  for word in text:
+    if word.isalnum() and word not in stopwords.words('english') and word not in string.punctuation:
+      y.append(ps.stem(word))
+  return " ".join(y)
+
+@app.route('/',methods=['GET','POST'])
+def predict_spam():
+    text = ""
+    prediction = ""
+    if request.method == 'POST':
+        text = request.form.get('sms')
+        transformed_sms = transform_text(text)
+        vector_sms = tfidf.transform([transformed_sms])
+        y_pred = model.predict(vector_sms)[0]
+        prediction =  "spam" if y_pred == 1 else "ham"
+    return render_template('home.html', text=text,prediction = prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
